683b94a172ae7c1917c0496c39cfdd63329909a8
Khadim Dia <mozard2020@gmail.com>
2026-06-10 20:09:38 -0400

    chore: bootstrap enterprise rag pipeline

---
+++ src/main/java/ca/kevin/enterprise_rag_pipeline/EnterpriseRagPipelineApplication.java
@@ -0,0 +1,13 @@
+package ca.kevin.enterprise_rag_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseRagPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseRagPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/kevin/enterprise_rag_pipeline/EnterpriseRagPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.kevin.enterprise_rag_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseRagPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
